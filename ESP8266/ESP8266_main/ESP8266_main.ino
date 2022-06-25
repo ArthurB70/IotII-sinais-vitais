efd7cb245e1340130bae1775523993cbeabe4eb9
Arthur Moura <69174018+ArthurB70@users.noreply.github.com>
2022-06-25 15:00:48 -0300

    v_final

--- ESP8266/ESP8266_main/ESP8266_main.ino
+++ ESP8266/ESP8266_main/ESP8266_main.ino
@@ -1,115 +1,143 @@
+/*
+  Optical SP02 Detection (SPK Algorithm) using the MAX30105 Breakout
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 19th, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
+
+  This demo shows heart rate and SPO2 levels.
+
+  It is best to attach the sensor to your finger using a rubber band or other tightening 
+  device. Humans are generally bad at applying constant pressure to a thing. When you 
+  press your finger against the sensor it varies enough to cause the blood in your 
+  finger to flow differently which causes the sensor readings to go wonky.
+
+  This example is based on MAXREFDES117 and RD117_LILYPAD.ino from Maxim. Their example
+  was modified to work with the SparkFun MAX30105 library and to compile under Arduino 1.6.11
+  Please see license file for more info.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+ 
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
 #include <Wire.h>
-#include <Arduino.h>
-#include "algorithm_by_RF.h"
-#include "max30102.h"
-#include "algorithm.h"
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
 
-#define MAX_30102_ADDR 0X57
-const byte max30102_int_pin = 14; // GPIO14/D5
-uint32_t elapsedTime,timeStart;
+MAX30105 particleSensor;
 
-uint32_t aun_ir_buffer[BUFFER_SIZE]; //infrared LED sensor data
-uint32_t aun_red_buffer[BUFFER_SIZE];  //red LED sensor data
-float old_n_spo2;  // Previous SPO2 value
-uint8_t uch_dummy,k;
+#define MAX_BRIGHTNESS 255
 
-void millis_to_hours(uint32_t ms, char* hr_str)
+
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+
+void setup()
 {
-  char istr[6];
-  uint32_t secs,mins,hrs;
-  secs=ms/1000; // time in seconds
-  mins=secs/60; // time in minutes
-  secs-=60*mins; // leftover seconds
-  hrs=mins/60; // time in hours
-  mins-=60*hrs; // leftover minutes
-  itoa(hrs,hr_str,10);
-  strcat(hr_str,":");
-  itoa(mins,istr,10);
-  strcat(hr_str,istr);
-  strcat(hr_str,":");
-  itoa(secs,istr,10);
-  strcat(hr_str,istr);
-}
-int cont =0;
-void setup() {
+  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
 
-  pinMode(max30102_int_pin, INPUT);  //pin D10 connects to the interrupt output pin of the MAX30102
-  Serial.begin(115200);
-  delay(100);
-  maxim_max30102_init();  //initialize the MAX30102
-  old_n_spo2=0.0;
-  timeStart=millis();
+
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
+
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0) ; //wait until user presses a key
   Serial.read();
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 }
 
-//Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every ST seconds
-void loop() {
-  float n_spo2,ratio,correl;  //SPO2 value
-  int8_t ch_spo2_valid;  //indicator to show if the SPO2 calculation is valid
-  int32_t n_heart_rate; //heart rate value
-  int8_t  ch_hr_valid;  //indicator to show if the heart rate calculation is valid
-  int32_t i;
-  char hr_str[10];
-  if(cont <1000){
-    for(i=0;i<BUFFER_SIZE;i++, cont++)
-      {
-      while(digitalRead(max30102_int_pin)==1);  //wait until the interrupt pin asserts
-      maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
-  /*
-      Serial.print(i, DEC);
-      Serial.print(F("\t"));
-      Serial.print(aun_red_buffer[i], DEC);
-      Serial.print(F("\t"));
-      Serial.print(aun_ir_buffer[i], DEC);    
-      Serial.println("");*/
-      delay(5);
-      }    
+void loop()
+{
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
   }
-//param[in]    *pun_ir_buffer           - IR sensor data buffer
-//param[in]    n_ir_buffer_length      - IR sensor data buffer length
-//param[in]    *pun_red_buffer          - Red sensor data buffer
-//param[out]    *pn_spo2                - Calculated SpO2 value
-//param[out]    *pch_spo2_valid         - 1 if the calculated SpO2 value is valid
-//param[out]    *pn_heart_rate          - Calculated heart rate value
-//param[out]    *pch_hr_valid           - 1 if the calculated heart rate value is valid
-  maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid);
 
-   Serial.println("--RF--");
-  Serial.print(elapsedTime);
-  Serial.print("\t");
-  Serial.print(n_spo2);
-  Serial.print("\t");
-  Serial.print(n_heart_rate, DEC);
-  Serial.println("------");
-  n_spo2 = 0;
-  n_heart_rate= 0;
-  /*
-  //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using Robert's method
-  rf_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid, &ratio, &correl); 
-  elapsedTime=millis()-timeStart;
-  millis_to_hours(elapsedTime,hr_str); // Time in hh:mm:ss format
-  elapsedTime/=1000; // Time in seconds
-  // Read the _chip_ temperature in degrees Celsius
-  int8_t integer_temperature;
-  uint8_t fractional_temperature;
-  maxim_max30102_read_temperature(&integer_temperature, &fractional_temperature);
-  float temperature = integer_temperature + ((float)fractional_temperature)/16.0;
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-  Serial.println("--RF--");
-  Serial.print(elapsedTime);
-  Serial.print("\t");
-  Serial.print(n_spo2);
-  Serial.print("\t");
-  Serial.print(n_heart_rate, DEC);
-  Serial.print("\t");
-  Serial.print(hr_str);
-  Serial.print("\t");
-  Serial.println(temperature);
-  Serial.println("------");
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
 
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
 
-  if(ch_hr_valid && ch_spo2_valid) { 
-    old_n_spo2=n_spo2;
+      
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.print(spo2, DEC);
+
+      Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
   }
-  */
 }
--- ESP8266/ESP8266_test/ESP8266_test/ESP8266_test.ino
+++ ESP8266/ESP8266_test/ESP8266_test/ESP8266_test.ino
@@ -1,43 +1,297 @@
 #include <Wire.h>
 #include "MAX30105.h"
+#include "spo2_algorithm.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <time.h>
+#include <TZ.h>
+#include <FS.h>
+#include <LittleFS.h>
+#include <CertStoreBearSSL.h>
+#include <SparkFunMLX90614.h>
 
-MAX30105 particleSensor; // initialize MAX30102 with I2C
+const char* ssid = "NET VIRTUA 103";
+const char* password = "23342334";
+const char* mqtt_server = "ba87952a69c64e73a1569a69541d7fd6.s1.eu.hivemq.cloud";
+
+BearSSL::CertStore certStore;
+
+WiFiClientSecure espClient;
+PubSubClient * client;
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (500)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+
+// mqtt user: iot-vital-signs-user
+// mqtt password: #Po23aa.14sz
+IRTherm therm;
+MAX30105 max30102;
+
+int32_t bufferLength = 100; 
+int32_t oxigenacao; 
+int8_t v_oxigenacao; 
+int32_t batimentoCardiaco; 
+int8_t v_batimentoCardiaco; 
+
+float batimentoCardiaco_avg = 0; 
+float oxigenacao_avg = 0; 
+float temp_avg = 0;
+int i_hr = 0, i_spo = 0, i_temp = 0;
+int32_t milli_segundos = 16000; 
+
+uint32_t irBuffer[100];
+uint32_t redBuffer[100];
+
+uint32_t inicio_tempo;
+
+
+String callback_retorno = "";
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void setDateTime() {
+  // You can use your own timezone, but the exact time is not used at all.
+  // Only the date is needed for validating the certificates.
+  configTime(TZ_Europe_Berlin, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(100);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println();
+
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.printf("%s %s", tzname[0], asctime(&timeinfo));
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+  callback_retorno = "";
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    callback_retorno += (char)payload[i];
+  }
+  Serial.println();
+
+  // Switch on the LED if the first character is present
+  if ((char)payload[0] != NULL) {
+    digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  }
+}
+void reconnect() {
+  // Loop until we’re reconnected
+  while (!client->connected()) {
+    Serial.print("Attempting MQTT connection…");
+    String clientId = "ESP8266Client - MyClient";
+    // Attempt to connect
+    // Insert your password
+    if (client->connect(clientId.c_str(), "iot-vital-signs-user", "#Po23aa.14sz")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement…
+      client->publish("testTopic", "device: connected");
+      // … and resubscribe
+      client->subscribe("testTopic");
+    } else {
+      Serial.print("failed, rc = ");
+      Serial.print(client->state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup()
 {
-  Serial.begin(115200);
-  while(!Serial); //We must wait for Teensy to come online
-  delay(100);
-  Serial.println("");
-  Serial.println("MAX30102");
-  Serial.println("");
-  delay(100);
-  // Initialize sensor
-  if (particleSensor.begin(Wire) == false) //Use default I2C port, 400kHz speed
-  {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
-    while (1);
+  Serial.begin(115200); 
+  Serial.println("Conecando MAX30102 . . .");
+  while (!max30102.begin(Wire, I2C_SPEED_STANDARD));
+  Serial.println("MAX30102 conectado!");  
+  
+  
+  byte ledBrightness = 60;
+  byte sampleAverage = 4;
+  byte ledMode = 2;
+  byte sampleRate = 100; 
+  int pulseWidth = 411; 
+  int adcRange = 4096;
+  Serial.println("Inicializando MAX30102 . . ."); 
+  max30102.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+  Serial.println("MAX30102 inicializado!"); 
+  Wire.begin();
+  if (therm.begin() == false){ // Initialize thermal IR sensor
+    Serial.println("Qwiic IR thermometer did not acknowledge! Freezing!");
+    while(1);
+  }
+  Serial.println("Qwiic IR Thermometer did acknowledge.");
+  
+  therm.setUnit(TEMP_C); // Set the library's units to Farenheit
+  // Alternatively, TEMP_F can be replaced with TEMP_C for Celsius or
+  // TEMP_K for Kelvin.
+    delay(500);
+
+  LittleFS.begin();
+  setup_wifi();
+  setDateTime();
+
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
+
+  // you can use the insecure mode, when you want to avoid the certificates
+  //espclient->setInsecure();
+
+  int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+  Serial.printf("Number of CA certs read: %d\n", numCerts);
+  if (numCerts == 0) {
+    Serial.printf("No certs found. Did you run certs-from-mozilla.py and upload the LittleFS directory before running?\n");
+    return; // Can't connect to anything w/o certs!
   }
 
-  byte ledBrightness = 70; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 69; //Options: 69, 118, 215, 411
-  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
+  // Integrate the cert store with this connection
+  bear->setCertStore(&certStore);
 
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  client = new PubSubClient(*bear);
+
+  client->setServer(mqtt_server, 8883);
+  client->setCallback(callback);
+  inicio_tempo = millis();
 }
 
-void loop() {
-  particleSensor.check(); //Check the sensor
-  while (particleSensor.available()) {
-      // read stored IR
-      Serial.print(particleSensor.getFIFOIR());
-      Serial.print(",");
-      // read stored red
-      Serial.println(particleSensor.getFIFORed());
-      // read next set of samples
-      particleSensor.nextSample();      
+void loop()
+{
+
+  
+  if (!client->connected()) {
+    reconnect();
+  }
+  else {
+    if(callback_retorno == "teste"){
+      batimentoCardiaco_avg = oxigenacao_avg = temp_avg = 0; 
+      i_hr = i_spo = i_temp = 0;
+      for (byte i = 0 ; i < bufferLength ; i++)
+      {
+        while (max30102.available() == false) 
+          max30102.check();
+        redBuffer[i] = max30102.getRed();
+        irBuffer[i] = max30102.getIR();
+        max30102.nextSample(); 
+      }
+      maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &oxigenacao, &v_oxigenacao, &batimentoCardiaco, &v_batimentoCardiaco);
+      //Continuously taking samples from MAX30102.  Heart rate and oxigenacao are calculated every 1 second
+      while ((millis() - inicio_tempo + 4000 < milli_segundos) || (i_hr <= (((milli_segundos/1000)/4) - 1) && i_spo <= (((milli_segundos/1000)/4) - 1)))
+      {
+      
+        for (byte i = 0; i < 100; i++)
+        {
+          while (max30102.available() == false)
+            max30102.check(); 
+          redBuffer[i] = max30102.getRed();
+          irBuffer[i] = max30102.getIR();
+          max30102.nextSample(); 
+        }
+        maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &oxigenacao, &v_oxigenacao, &batimentoCardiaco, &v_batimentoCardiaco);
+        
+        if (therm.read()) // On success, read() will return 1, on fail 0.
+        {
+          float temp = therm.object();
+          temp_avg += temp;
+          Serial.print(F("->temp="));
+          Serial.println(temp);
+          i_temp++;
+        }
+        if(v_oxigenacao = 1 && oxigenacao >= 70 && oxigenacao <= 100){
+          oxigenacao_avg += oxigenacao;
+          Serial.print(F("->oxigenacao="));
+          Serial.println(oxigenacao);
+          i_spo++;
+        }
+        if(v_batimentoCardiaco = 1 && batimentoCardiaco >= 40 && batimentoCardiaco <= 180){
+          batimentoCardiaco_avg += batimentoCardiaco;
+          Serial.print(F("->hr="));
+          Serial.println(batimentoCardiaco);
+          i_hr++;
+        } 
+        if(i_spo > 0 || i_hr > 0 || i_temp > 0){
+          String aux = "";
+          aux = "";
+          aux += "device(p): ";
+          aux += String(oxigenacao_avg/i_spo);
+          aux += ",";
+          aux += String(batimentoCardiaco_avg/i_hr);
+          aux += ",";
+          aux += String(temp_avg/i_temp);
+          aux.toCharArray(msg, MSG_BUFFER_SIZE);
+          //snprintf (msg, MSG_BUFFER_SIZE,retorno, );
+          Serial.print("Publish message: ");
+          Serial.println(msg);
+          if (!client->connected()) {
+            reconnect();
+          }
+          else{
+            client->publish("testTopic", msg);
+          }
+        }
+      }
+        String aux = "";
+    
+        aux += "device(f): ";
+        aux += String(oxigenacao_avg/i_spo);
+        aux += ",";
+        aux += String(batimentoCardiaco_avg/i_hr);
+        aux += ",";
+        aux += String(temp_avg/i_temp);
+        //snprintf (msg, MSG_BUFFER_SIZE, "%s", retorno);
+        aux.toCharArray(msg, MSG_BUFFER_SIZE);
+        Serial.print("Publish message: ");
+        Serial.println(msg);
+        if (!client->connected()) {
+            reconnect();
+          }
+          else{
+            client->publish("testTopic", msg);
+        }
+        Serial.print(F("M->oxigenacao="));
+        Serial.println(oxigenacao_avg/i_spo);
+      
+        Serial.print(F("M->hr="));
+        Serial.println(batimentoCardiaco_avg/i_hr);
+    
+    }
+    else {
+      client->loop();
+    }
   }
 }
